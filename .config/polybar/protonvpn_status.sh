2d9272767a16c6eec053778dae53a5d2a894b5ee
Bob Henkel <bob.henkel@gmail.com>
2021-07-07 22:19:59 -0500

    Update protonvpn_status.sh
    Get rid of original content that I used as an example to help figure things out.

--- .config/polybar/protonvpn_status.sh
+++ .config/polybar/protonvpn_status.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 
-# if [ $(herbstclient list_monitors | grep 2: | cut -d' ' -f2 | cut -d'x' -f1) == 2900 ];
-# then echo "21x9";
-# else echo "16x9"
-# fi
 source .bash_profile #password is stored here
 protonvpn_status=$(echo $SUDO_PASSWORD | sudo -S protonvpn s | grep "Status:")
 
@@ -13,5 +9,8 @@ then
 else
 	echo VPN:DOWN
 fi
+
+
+
 
 
